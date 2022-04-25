4aafc92ae833726e8332df5dfc6bbb6c22d0df92
Jinheon-Park <wlsgjs2798@ajou.ac.kr>
2022-04-25 10:15:11 +0900

    fun

--- study.js
+++ study.js
@@ -1,1 +1,18 @@
-console.log("Hellow World!")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  let input = parseInt(line);
+  let sum = 0;
+  for (let i = 1; i <= input; i++) sum += i;
+  console.log(sum);
+  rl.close();
+});
+
+rl.on("close", () => {
+  process.exit();
+});
